b5eea5881a04b54ca10b3ea2cf536fe943c94176
mmsd7 <yousuf.py7@gmail.com>
2024-04-14 15:42:04 +0600

    Create Database, Migration, Writing and Reading Data using shell

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
-
-HTML_STRING = """
-    <h1> Hello World!</h1>
-"""
-
+import random
 
 def home_view(request):
+    name = 'Yousuf'
+    number = random.randint(1, 5000)
+    
+    H_STRING = f"""
+    <h1> Hello {name}- Here is the number: {number}</h1>
+    """
+    P_STRING = f"""
+    <p> Hello {name}- Here is the number: {number}</p>
+    """
+    
+    HTML_STRING = H_STRING + P_STRING
+
+
     return HttpResponse(HTML_STRING)
