6706aab5dc8e02f6918d1b3698793b0b74dc9c96
mmsd7 <yousuf.py7@gmail.com>
2024-04-15 18:55:11 +0600

    23. Basic HTML form in Django

--- articles/views.py
+++ articles/views.py
@@ -14,6 +14,17 @@ def article_search_view(request):
     context = {'object':article_obj}
     return render(request, 'articles/search.html', context=context)
 
+def article_create_view(request):
+    context = {}
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article_obj = Article.objects.create(title= title, content=content)
+        context['object'] = article_obj
+        context['created'] = True
+    return render (request, 'articles/create.html', context=context)
+
+
 
 def article_detail_view(request, id=None):
     article_obj = None
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from .views import home_view
 from articles.views import article_detail_view
 from articles.views import article_search_view
+from articles.views import article_create_view
 
 
 
 urlpatterns = [
     path('', home_view),
     path('articles/', article_search_view),
+    path('articles/create/', article_create_view),
     path('articles/<int:id>/', article_detail_view),
     path('admin/', admin.site.urls),
 ]
