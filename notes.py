77e70c893749b8ec54b6f48d93d6935205baa136
mmsd7 <yousuf.py7@gmail.com>
2024-04-22 21:49:09 +0600

    44 - get absolute url

--- notes.py
+++ notes.py
@@ -1,123 +1,54 @@
-# 43 - Slugs in Dynamic Urls
-# Working on trydjango app
-# Here is the code we have in urls.py
-urlpatterns = [
-    path('', home_view),
-    path('articles/', article_search_view),
-    path('articles/create/', article_create_view),
-    path('articles/<int:id>/', article_detail_view),
-    path('admin/', admin.site.urls),
-    path('login/', login_view ),
-    path('logout/', logout_view),
-    path('register/', register_view),
-]
+# 44 - get absolute url
+# in articles.models
+import random
+from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.utils import timezone
+from django.utils.text import slugify
 
-# Now let's make some changes
-urlpatterns = [
-    path('', home_view),
-    path('articles/', article_search_view),
-    path('articles/create/', article_create_view),
-    path('articles/<slug:slug>/', article_detail_view),
-    path('admin/', admin.site.urls),
-    path('login/', login_view ),
-    path('logout/', logout_view),
-    path('register/', register_view),
-]
+from .utils import slugify_instance_title
 
-# Our article.article_detail_views file is like below
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True, blank=True, null = True)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add = True)
+    update = models.DateTimeField(auto_now = True)
+    publish = models.DateField(auto_now_add = False, auto_now = False, null=True, blank = True)
 
-def article_detail_view(request, id=None):
-    article_obj = None
-    if id is not None:
-        article_obj = Article.objects.get(id=id)
-    context={
-        'object': article_obj,
-    }
-    return render(request, 'articles/detail.html', context =  context)
+    def save(self, *args, **kwargs):
+        #obj = Article.objects.get(id=5)
+        #set something
+        # if self.slug is None:
+        #     self.slug = slugify(self.title) 
+        super().save(*args, **kwargs)
+        #obj.save()
+        #Do anothe rsomething
 
-# Now let's make some changes
+def article_pre_save(sender, instance, *args, **kwargs):
+    print('pre_save')
+    if instance.slug is None:
+        slugify_instance_title(instance, save=False)
+    
 
-def article_detail_view(request, slug=None): #MD
-    article_obj = None
-    if slug is not None:  #MD
-        article_obj = Article.objects.get(id=slug)  #MD
-    context={
-        'object': article_obj,
-    }
-    return render(request, 'articles/detail.html', context =  context)
-# Now let's make some changes
+pre_save.connect(article_pre_save, sender = Article)
 
-def article_detail_view(request, slug=None):
-    article_obj = None
-    if slug is not None:
-        article_obj = Article.objects.get(slug=slug)  #MD
-    context={
-        'object': article_obj,
-    }
-    return render(request, 'articles/detail.html', context =  context)
+def article_post_save(sender, instance, created, *args, **kwargs):
+    print('post_save')
+    if created:
+        slugify_instance_title(instance, save=True)
 
-# +++++++++++++++++++++++
-def article_detail_view(request, slug=None):
-    article_obj = None
-    if slug is not None:
-        article_obj = Article.objects.get(slug=slug)
-    context={
-        'object': article_obj,
-    }
-    return render(request, 'articles/detail.html', context =  context)
+    
+post_save.connect(article_post_save, sender = Article)
 
-# TRY, It works fine!!
+===============================
+# Add get_absolute_url function
 
-# +++++++++++++++++++++++
-iport Http404 #NA
-def article_detail_view(request, slug=None):
-    article_obj = None
-    if slug is not None:
-        try: #NA
-            article_obj = Article.objects.get(slug=slug)
-        except:   #NA
-            pass #NA
-    context={
-        'object': article_obj,
-    }
-    return render(request, 'articles/detail.html', context =  context)
-# +++++++++++++++++++++++
+def get_absolute_url(self):
+    return f'/article/{self.slug}/'
 
-
-def article_detail_view(request, slug=None):
-    article_obj = None
-    if slug is not None:
-        try: 
-            article_obj = Article.objects.get(slug=slug)
-        except Article.DoesNotExist:
-            raise Http404
-        except Article.MultipleObjectsReturned:
-            article_obj = Article.objects.filter(slug=slug).first()
-        except:
-            raise Http404
-    context={
-        'object': article_obj,
-    }
-    return render(request, 'articles/detail.html', context =  context)
-
-=============================================
-def article_detail_view(request, slug=None):
-    article_obj = None
-    if slug is not None:
-        try: 
-            article_obj = Article.objects.get(slug=slug)
-        except Article.DoesNotExist:
-            raise Http404
-        except Article.MultipleObjectsReturned:
-            article_obj = Article.objects.filter(slug=slug).first()
-        except:
-            raise Http404
-    context={
-        'object': article_obj,
-    }
-    return render(request, 'articles/detail.html', context =  context)
-
-# home_view.html
+# in home_view.html
 {% extends 'base.html' %}
 
 {% block content %}
@@ -130,7 +61,7 @@ def article_detail_view(request, slug=None):
     {% for x in object_list %}
         {% if x.title %}    
             <li> 
-                <a href="/articles/{{ x.slug }}/"> {{ x.title }} - {{ x.content }} </a> #Modified
+                <a href="{{ x.get_absolute_url }}"> {{ x.title }} - {{ x.content }} </a> 
             </li>
         {% endif %}
     {% endfor %}
