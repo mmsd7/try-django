814ca2fa150b6dd33b3a2a4ab90161bd6ec4e32c
mmsd7 <yousuf.py7@gmail.com>
2024-04-22 19:42:52 +0600

    40 - Auto Generate Slugs with Recursion

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,15 @@
+import random
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.utils import timezone
 from django.utils.text import slugify
 
+from .utils import slugify_instance_title
+
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField(blank=True, null = True)
+    slug = models.SlugField(unique=True, blank=True, null = True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add = True)
     update = models.DateTimeField(auto_now = True)
@@ -20,17 +23,6 @@ class Article(models.Model):
         super().save(*args, **kwargs)
         #obj.save()
         #Do anothe rsomething
-
-def slugify_instance_title(instance, save=False):
-    slug = slugify(instance.title) 
-    qs = Article.objects.filter(slug=slug).exclude(id=instance.id)
-    if qs.exists():
-        slug = f"{slug}- {qs.count() + 1}"
-    instance.slug = slug
-    if save:
-        instance.save()
-    return instance
-
 
 def article_pre_save(sender, instance, *args, **kwargs):
     print('pre_save')
