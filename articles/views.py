7de4122197ca1498540718d6b2efb0e3a7e87567
mmsd7 <yousuf.py7@gmail.com>
2024-04-22 21:36:51 +0600

    43 - Slugs in Dynamic Urls

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import Http404
 from articles.models import Article
 from .forms import ArticleForm
 
@@ -31,10 +32,17 @@ def article_create_view(request):
 
 
 
-def article_detail_view(request, id=None):
+def article_detail_view(request, slug=None):
     article_obj = None
-    if id is not None:
-        article_obj = Article.objects.get(id=id)
+    if slug is not None:
+        try: 
+            article_obj = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404
+        except Article.MultipleObjectsReturned:
+            article_obj = Article.objects.filter(slug=slug).first()
+        except:
+            raise Http404
     context={
         'object': article_obj,
     }
--- notes.py
+++ notes.py
@@ -1,41 +1,141 @@
-# 30. Login via django authentication form
-# Import authenticationForm
-# Here is the login_view in accounts.views.py
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        print(username, password)
-        user = authenticate(request, username= username, password = password)
-        if user is None:
-            context = {"error": 'Invalid username or password.'}
-            return render (request, 'accounts/login.html', context=context)
-        login(request, user)
-        return redirect('/')
-    return render(request, 'accounts/login.html', {})
+# 43 - Slugs in Dynamic Urls
+# Working on trydjango app
+# Here is the code we have in urls.py
+urlpatterns = [
+    path('', home_view),
+    path('articles/', article_search_view),
+    path('articles/create/', article_create_view),
+    path('articles/<int:id>/', article_detail_view),
+    path('admin/', admin.site.urls),
+    path('login/', login_view ),
+    path('logout/', logout_view),
+    path('register/', register_view),
+]
 
-# Change the code like the following
+# Now let's make some changes
+urlpatterns = [
+    path('', home_view),
+    path('articles/', article_search_view),
+    path('articles/create/', article_create_view),
+    path('articles/<slug:slug>/', article_detail_view),
+    path('admin/', admin.site.urls),
+    path('login/', login_view ),
+    path('logout/', logout_view),
+    path('register/', register_view),
+]
 
-def login_view(request):
-    if request.method == 'POST':
-        form = authenticationForm(request, data = request.POST)
-        if form.is_valid():
-            user = form.get_user() #It's special for his particular form
-            login(request, user)
-            return redirect('/')
-        else:
-            form = authenticationForm(request)
-        return render(request, 'accounts/login.html', {})
+# Our article.article_detail_views file is like below
 
-# Check! 
-def login_view(request):
-    if request.method == 'POST':
-        form = authenticationForm(request, data = request.POST)
-        if form.is_valid():
-            user = form.get_user() #It's special for his particular form
-            login(request, user)
-            return redirect('/')
-        else:
-            form = authenticationForm(request)
-        context = {'form':form}
-        return render(request, 'accounts/login.html', context =  context)
+def article_detail_view(request, id=None):
+    article_obj = None
+    if id is not None:
+        article_obj = Article.objects.get(id=id)
+    context={
+        'object': article_obj,
+    }
+    return render(request, 'articles/detail.html', context =  context)
+
+# Now let's make some changes
+
+def article_detail_view(request, slug=None): #MD
+    article_obj = None
+    if slug is not None:  #MD
+        article_obj = Article.objects.get(id=slug)  #MD
+    context={
+        'object': article_obj,
+    }
+    return render(request, 'articles/detail.html', context =  context)
+# Now let's make some changes
+
+def article_detail_view(request, slug=None):
+    article_obj = None
+    if slug is not None:
+        article_obj = Article.objects.get(slug=slug)  #MD
+    context={
+        'object': article_obj,
+    }
+    return render(request, 'articles/detail.html', context =  context)
+
+# +++++++++++++++++++++++
+def article_detail_view(request, slug=None):
+    article_obj = None
+    if slug is not None:
+        article_obj = Article.objects.get(slug=slug)
+    context={
+        'object': article_obj,
+    }
+    return render(request, 'articles/detail.html', context =  context)
+
+# TRY, It works fine!!
+
+# +++++++++++++++++++++++
+iport Http404 #NA
+def article_detail_view(request, slug=None):
+    article_obj = None
+    if slug is not None:
+        try: #NA
+            article_obj = Article.objects.get(slug=slug)
+        except:   #NA
+            pass #NA
+    context={
+        'object': article_obj,
+    }
+    return render(request, 'articles/detail.html', context =  context)
+# +++++++++++++++++++++++
+
+
+def article_detail_view(request, slug=None):
+    article_obj = None
+    if slug is not None:
+        try: 
+            article_obj = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404
+        except Article.MultipleObjectsReturned:
+            article_obj = Article.objects.filter(slug=slug).first()
+        except:
+            raise Http404
+    context={
+        'object': article_obj,
+    }
+    return render(request, 'articles/detail.html', context =  context)
+
+=============================================
+def article_detail_view(request, slug=None):
+    article_obj = None
+    if slug is not None:
+        try: 
+            article_obj = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404
+        except Article.MultipleObjectsReturned:
+            article_obj = Article.objects.filter(slug=slug).first()
+        except:
+            raise Http404
+    context={
+        'object': article_obj,
+    }
+    return render(request, 'articles/detail.html', context =  context)
+
+# home_view.html
+{% extends 'base.html' %}
+
+{% block content %}
+
+<h1>{{ object.title }} ({{ object.id }})</h1>
+<p> {{ object.content }} </p>
+
+
+<ul>
+    {% for x in object_list %}
+        {% if x.title %}    
+            <li> 
+                <a href="/articles/{{ x.slug }}/"> {{ x.title }} - {{ x.content }} </a> #Modified
+            </li>
+        {% endif %}
+    {% endfor %}
+
+
+</ul>
+
+{% endblock %}
